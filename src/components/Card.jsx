96dd768eb637180124e41041dde53b624bc9302a
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-04-19 19:51:06 -0400

    Add: card de mach

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import Card from "react-bootstrap/Card";
+import logo from "../assets/images/logo-mach.png";
+function MachIgm() {
+  return (
+    <Card className="bg-dark ">
+      <Card.Img src={logo} alt="Card image" width="10%"/>
+    </Card>
+  );
+}
+
+export default MachIgm;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,29 +2,32 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
-
+import MachIgm from "../components/Card";
 function Login() {
   return (
-    <Form>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Email
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue="email@example.com" />
-        </Col>
-      </Form.Group>
+    <>
+      <MachIgm />
+      <Form>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+          <Form.Label column sm="2">
+            Email
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control plaintext readOnly defaultValue="email@example.com" />
+          </Col>
+        </Form.Group>
 
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="2">
-          Password
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
-        </Col>
-        <Button variant="primary">Ingresar</Button>{" "}
-      </Form.Group>
-    </Form>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+          <Form.Label column sm="2">
+            Password
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+          <Button variant="primary">Ingresar</Button>{" "}
+        </Form.Group>
+      </Form>
+    </>
   );
 }
 
