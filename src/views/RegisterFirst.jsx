e9b345d340615475d47ad7b27cfb0cfa33a85519
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-04-20 10:40:09 -0400

    feat: adding register 1 step

--- src/views/RegisterFirst.jsx
+++ src/views/RegisterFirst.jsx
@@ -1,21 +1,48 @@
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import MachIgm from "../components/Card";
+
 const RegisterFirst = () => {
+	const [name, setName] = useState("");
+	const [lastname, setLastname] = useState("");
+
+	const handleNameChange = (event) => {
+		setName(event.target.value);
+	};
+	const handleLastnameChange = (event) => {
+		setLastname(event.target.value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Aquí puedes enviar los datos del formulario a través de una solicitud HTTP
+		console.log("name:", name);
+		console.log("lastname:", lastname);
+	};
 	return (
-		<main className="register">
-			<section className="register_first">
-				<form>
-					<input
-						type="text"
-						className="register_name"
-						placeholder="Ingresa tu nombre"
-					/>
-					<input
-						type="text"
-						className="register_lastname"
-						placeholder="Ingresa tu apellido"
-					/>
-				</form>
-			</section>
-		</main>
+		<>
+			<MachIgm />
+			<h2>Regístrate en pocos pasos</h2>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					label="Nombre"
+					value={name}
+					onChange={handleNameChange}
+					fullWidth
+					margin="normal"
+					placeholder="Ingresa tu nombre"
+				/>
+				<TextField
+					label="Lastname"
+					value={lastname}
+					onChange={handleLastnameChange}
+					fullWidth
+					margin="normal"
+				/>
+				<Button type="submit" variant="contained" color="primary" fullWidth>
+					Continuar
+				</Button>
+			</form>
+		</>
 	);
 };
 
