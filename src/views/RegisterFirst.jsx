38a671a9392d45b1f2dbaa0a5ae09de01ddd042f
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-04-20 11:57:21 -0400

    feat: adding rut validator

--- src/views/RegisterFirst.jsx
+++ src/views/RegisterFirst.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Typography } from "@mui/material";
 import MachIgm from "../components/Card";
 
 const RegisterFirst = () => {
 	const [name, setName] = useState("");
 	const [lastname, setLastname] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
 
 	const handleNameChange = (event) => {
 		setName(event.target.value);
@@ -12,16 +14,27 @@ const RegisterFirst = () => {
 	const handleLastnameChange = (event) => {
 		setLastname(event.target.value);
 	};
+	const handlePhoneChange = (event) => {
+		setPhone(event.target.value);
+	};
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		// Aquí puedes enviar los datos del formulario a través de una solicitud HTTP
 		console.log("name:", name);
 		console.log("lastname:", lastname);
+		console.log("telefono:", phone);
+		console.log("email:", email);
 	};
+
 	return (
 		<>
 			<MachIgm />
-			<h2>Regístrate en pocos pasos</h2>
+			<Typography variant="h5" align="center" gutterBottom>
+				Regístrate en pocos pasos
+			</Typography>
 			<form onSubmit={handleSubmit}>
 				<TextField
 					label="Nombre"
@@ -35,6 +48,21 @@ const RegisterFirst = () => {
 					label="Lastname"
 					value={lastname}
 					onChange={handleLastnameChange}
+					fullWidth
+					margin="normal"
+				/>
+				<TextField
+					label="Teléfono"
+					value={phone}
+					onChange={handlePhoneChange}
+					fullWidth
+					margin="normal"
+					placeholder="Ingresa tu nombre"
+				/>
+				<TextField
+					label="e-mail"
+					value={email}
+					onChange={handleEmailChange}
 					fullWidth
 					margin="normal"
 				/>
