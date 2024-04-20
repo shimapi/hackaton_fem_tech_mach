ba13c0d23c6881dc5ee27405b69a151eed4dac32
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-04-20 15:55:28 -0400

    feat: adding trans mov

--- src/views/Movimientos.jsx
+++ src/views/Movimientos.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -5,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Container, Typography } from "@mui/material";
+import { Container, Pagination, Typography } from "@mui/material";
 import MachIgm from "../components/Card";
 
 function createData(date, payment, amount) {
@@ -18,9 +19,31 @@ const rows = [
 	createData("25 Mayo 2024", "Compra en Zara", "$ 30.990"),
 	createData("29 Junio 2024", "Compra en J&N", "$ 24.310"),
 	createData("30 Junio 2024", "Compra en H&M", "$ 54.330"),
+	createData("17 Marzo 2024", "Compra en OXXO", "$ 12.320"),
+	createData("20 Abril 2024", "Ahorro para Casa", "$ 120.000"),
+	createData("25 Mayo 2024", "Compra en Zara", "$ 30.990"),
+	createData("29 Junio 2024", "Compra en J&N", "$ 24.310"),
+	createData("30 Junio 2024", "Compra en H&M", "$ 54.330"),
+	createData("17 Marzo 2024", "Compra en OXXO", "$ 12.320"),
+	createData("20 Abril 2024", "Ahorro para Casa", "$ 120.000"),
+	createData("25 Mayo 2024", "Compra en Zara", "$ 30.990"),
+	createData("29 Junio 2024", "Compra en J&N", "$ 24.310"),
+	createData("30 Junio 2024", "Compra en H&M", "$ 54.330"),
+	createData("17 Marzo 2024", "Compra en OXXO", "$ 12.320"),
+	createData("20 Abril 2024", "Ahorro para Casa", "$ 120.000"),
+	createData("25 Mayo 2024", "Compra en Zara", "$ 30.990"),
+	createData("29 Junio 2024", "Compra en J&N", "$ 24.310"),
+	createData("30 Junio 2024", "Compra en H&M", "$ 54.330"),
 ];
 
 export default function Movimientos() {
+	const [page, setPage] = useState(1);
+	const itemsPerPage = 5;
+
+	const handleChange = (event, value) => {
+		setPage(value);
+		console.log(value);
+	};
 	return (
 		<Container maxWidth="sm" style={{ marginTop: "50px" }}>
 			<MachIgm />
@@ -29,7 +52,7 @@ export default function Movimientos() {
 			</Typography>
 			<TableContainer component={Paper}>
 				<Table aria-label="simple table">
-					<TableHead backgroundColor="secondary">
+					<TableHead>
 						<TableRow>
 							<TableCell>Fecha</TableCell>
 							<TableCell align="left">Movimiento</TableCell>
@@ -37,21 +60,29 @@ export default function Movimientos() {
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{rows.map((row) => (
-							<TableRow
-								key={row.date}
-								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-							>
-								<TableCell component="th" scope="row">
-									{row.date}
-								</TableCell>
-								<TableCell align="right">{row.payment}</TableCell>
-								<TableCell align="right">{row.amount}</TableCell>
-							</TableRow>
-						))}
+						{rows
+							.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+							.map((row) => (
+								<TableRow
+									key={Math.random()}
+									sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+								>
+									<TableCell component="th" scope="row">
+										{row.date}
+									</TableCell>
+									<TableCell align="right">{row.payment}</TableCell>
+									<TableCell align="right">{row.amount}</TableCell>
+								</TableRow>
+							))}
 					</TableBody>
 				</Table>
 			</TableContainer>
+			<br />
+			<Pagination
+				count={Math.ceil(rows.length / itemsPerPage)}
+				color="primary"
+				onChange={handleChange}
+			/>
 		</Container>
 	);
 }
--- src/views/Transferencias.jsx
+++ src/views/Transferencias.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -5,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Container, Typography } from "@mui/material";
+import { Container, Pagination, Typography } from "@mui/material";
 import MachIgm from "../components/Card";
 
 function createData(date, payment, amount) {
@@ -17,9 +18,31 @@ const rows = [
 	createData("20 Abril 2024", "Claudia Inostroza", "$ 10.000"),
 	createData("25 Mayo 2024", "Juanita Perez", "$ 50.000"),
 	createData("30 Junio 2024", "Margarita Perez", "$ 10.000"),
+	createData("17 Marzo 2024", "Juanita Perez", "$ 100.000"),
+	createData("20 Abril 2024", "Claudia Inostroza", "$ 10.000"),
+	createData("25 Mayo 2024", "Juanita Perez", "$ 50.000"),
+	createData("30 Junio 2024", "Margarita Perez", "$ 10.000"),
+	createData("17 Marzo 2024", "Juanita Perez", "$ 100.000"),
+	createData("20 Abril 2024", "Claudia Inostroza", "$ 10.000"),
+	createData("25 Mayo 2024", "Juanita Perez", "$ 50.000"),
+	createData("30 Junio 2024", "Margarita Perez", "$ 10.000"),
+	createData("20 Abril 2024", "Claudia Inostroza", "$ 10.000"),
+	createData("25 Mayo 2024", "Juanita Perez", "$ 50.000"),
+	createData("30 Junio 2024", "Margarita Perez", "$ 10.000"),
+	createData("17 Marzo 2024", "Juanita Perez", "$ 100.000"),
+	createData("20 Abril 2024", "Claudia Inostroza", "$ 10.000"),
+	createData("25 Mayo 2024", "Juanita Perez", "$ 50.000"),
+	createData("30 Junio 2024", "Margarita Perez", "$ 10.000"),
 ];
 
 export default function Transferencias() {
+	const [page, setPage] = useState(1);
+	const itemsPerPage = 5;
+
+	const handleChange = (event, value) => {
+		setPage(value);
+		console.log(value);
+	};
 	return (
 		<Container maxWidth="sm" style={{ marginTop: "50px" }}>
 			<MachIgm />
@@ -28,7 +51,7 @@ export default function Transferencias() {
 			</Typography>
 			<TableContainer component={Paper}>
 				<Table aria-label="simple table">
-					<TableHead backgroundColor="secondary">
+					<TableHead>
 						<TableRow>
 							<TableCell>Fecha</TableCell>
 							<TableCell align="left">Pago enviado</TableCell>
@@ -36,21 +59,28 @@ export default function Transferencias() {
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{rows.map((row) => (
-							<TableRow
-								key={row.date}
-								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-							>
-								<TableCell component="th" scope="row">
-									{row.date}
-								</TableCell>
-								<TableCell align="right">{row.payment}</TableCell>
-								<TableCell align="right">{row.amount}</TableCell>
-							</TableRow>
-						))}
+						{rows
+							.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+							.map((row) => (
+								<TableRow
+									key={Math.random()}
+									sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+								>
+									<TableCell component="th" scope="row">
+										{row.date}
+									</TableCell>
+									<TableCell align="right">{row.payment}</TableCell>
+									<TableCell align="right">{row.amount}</TableCell>
+								</TableRow>
+							))}
 					</TableBody>
 				</Table>
 			</TableContainer>
+			<Pagination
+				count={Math.ceil(rows.length / itemsPerPage)}
+				color="primary"
+				onChange={handleChange}
+			/>
 		</Container>
 	);
 }
